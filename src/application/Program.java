56223ddd8cf1d9c087e5d503402877cd97f3b368
paren <parentero0@gmail.com>
2020-07-11 02:42:58 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -29,7 +29,6 @@ public class Program {
 		System.out.println();
 		
 		System.out.println("=== Test #3: seller findAll ===");
-		department = new Department(2, null);
 		list = sellerDao.findAll();
 		for(Seller obj : list) {
 			System.out.println(obj);
@@ -40,6 +39,14 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted. New id = " + newSeller.getId());
+		System.out.println();
+		
+		System.out.println("=== Test #5: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
+		System.out.println();
 				
 	}
 	
